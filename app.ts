9f09f9dc2c4a292eef450a8fa074a1a15bdd6262
zouhang <zouhang@didiglobal.com>
2019-09-29 15:50:22 +0800

    :zap: improve code

--- app.ts
+++ app.ts
@@ -3,13 +3,13 @@ import { registerApplication, start } from 'single-spa'
 registerApplication(
   'vue',
   () => import('./src/vue/vue.app'),
-  () => location.pathname === "/react" ? false : true
+  () => !(location.pathname.startsWith('/react')),
 )
 
 registerApplication(
   'react',
   () => import('./src/react/react.app'),
-  () => location.pathname.startsWith('/vue')  ? false : true
+  () => !(location.pathname.startsWith('/vue')),
 )
 
 start()
